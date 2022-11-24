d27f8bc1e20936a1714c364192ca15c00a682f06
David Funnell <dfunnell77@gmail.com>
2022-11-23 18:19:48 -0800

    D3: Make It Interactive - Done

--- ghi/js/new-conference.js
+++ ghi/js/new-conference.js
@@ -1,25 +1,26 @@
 window.addEventListener('DOMContentLoaded', async () => {
 
-    const url = 'http://localhost:8000/api/states/';
+    const url = 'http://localhost:8000/api/locations/';
     const response = await fetch(url);
 
     if (response.ok) {
       const data = await response.json();
-      const selectTag = document.getElementById("state");
-      for (let state of data.states){
+      const selectTag = document.getElementById("location");
+      for (let location of data.locations){
         const option = document.createElement("option");
-        option.value = state.abbreviation;
-        option.innerHTML = state.name;
+        option.value = location.id;
+        option.innerHTML = location.name
         selectTag.appendChild(option)
       }
     }
 
-    const formTag = document.getElementById('create-location-form');
+
+    const formTag = document.getElementById('create-conference-form');
     formTag.addEventListener('submit', async event => {
       event.preventDefault();
       const formData = new FormData(formTag);
       const json = JSON.stringify(Object.fromEntries(formData));
-      const locationUrl = 'http://localhost:8000/api/locations/';
+      const locationUrl = 'http://localhost:8000/api/conferences/';
       const fetchConfig = {
         method: "post",
         body: json,
